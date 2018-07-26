9fb3cc3e672ba48ff189fd45ff384c812192c638
Tom Hipkin <tomhipkin@gmail.com>
2018-07-26 18:42:00 +0100

    (refactor) Welcome email moved to function
    * This makes us ready to send another type of email

--- send_email.rb
+++ send_email.rb
@@ -4,18 +4,22 @@ class SendEmail
   end
 
   def call
-    notify_client.send_email(
-      email_address: @user[:email],
-      template_id: ENV['NOTIFY_TEMPLATE_ID'],
-      personalisation: {
-        first_name: @user[:given_name],
-        school_name: @user[:school]
-      },
-      reference: 'welcome-to-teaching-jobs-email'
-    )
+    send_welcome_email
   end
 
   private def notify_client
     Notifications::Client.new(ENV['NOTIFY_KEY'])
+  end
+
+  private def send_welcome_email
+    notify_client.send_email(
+      email_address: @user[:email],
+      template_id: ENV['NOTIFY_WELCOME_TEMPLATE_ID'],
+      personalisation: {
+        first_name: @user[:given_name],
+        school_name: @user[:school_name]
+      },
+      reference: 'welcome-to-teaching-jobs-email'
+    )
   end
 end
