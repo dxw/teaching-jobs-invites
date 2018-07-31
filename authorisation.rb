027aed3966c1b88a2b9b4270adcb0223c4a63944
Robbie Paul <robbie.paul@dxw.com>
2018-07-31 17:45:39 +0100

    (feature) Users with multiple schools only receive one welcome email
    - Add permissions for each school belonging to a user.
    - Change the 'school name' in the welcome email if access has been given
    to multiple schools
    - Make sure our console output lists all the URNs access has been given
    to

--- authorisation.rb
+++ authorisation.rb
@@ -6,17 +6,18 @@ class Authorisation
   end
 
   def preauthorise
-    tva_response = tva_connection.post do |req|
-      req.url '/permissions'
-      req.headers['Authorization'] = "Token token=#{ENV['TVA_TOKEN']}"
-      req.headers['Content-Type'] = 'application/json'
-      req.body = JSON.generate(
-        user_token: @user[:email],
-        school_urn: @user[:school_urn]
-      )
+    @user[:schools].each do |school|
+      tva_response = tva_connection.post do |req|
+        req.url '/permissions'
+        req.headers['Authorization'] = "Token token=#{ENV['TVA_TOKEN']}"
+        req.headers['Content-Type'] = 'application/json'
+        req.body = JSON.generate(
+            user_token: @user[:email],
+            school_urn: school[:school_urn]
+        )
+      end
+      raise InvitationFailed, tva_response.body unless tva_response.success?
     end
-    raise InvitationFailed, tva_response.body unless tva_response.success?
-    tva_response
   end
 
   private def tva_connection
